b5b5c65deb206efac2b0eba4eb2786a6193e7e69
Sam Walker <sam.thompson.189@gmail.com>
2023-02-25 18:13:37 +1300

    Improve installation steps

--- macos/setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Hide "last login" line when starting a new terminal session
-touch $HOME/.hushlogin
-
-# Install IDE deps
-brew install neovim
-brew install ripgrep
---
+++ shell/.profile
@@ -0,0 +1,5 @@
+PATH=$HOME/.fnm:$PATH;
+PATH=$HOME/.cargo/env:PATH;
+PATH=$HOME/.config/composer/vendor/bin:$PATH
+
+eval "$(fnm env --use-on-cd)"
