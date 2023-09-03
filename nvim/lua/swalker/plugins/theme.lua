776dd0822dfe73d00032b38be797498f0843c78c
Sam Walker <sjwalker189@gmail.com>
2023-09-03 15:27:38 +1200

    UI tweaks

--- nvim/lua/swalker/plugins/theme.lua
+++ nvim/lua/swalker/plugins/theme.lua
@@ -1,9 +1,10 @@
 return {
-    {
-        'kvrohit/mellow.nvim',
-        priority = 1000,
-        config = function()
-            vim.cmd.colorscheme 'mellow'
-        end,
-    },
+  {
+    'kvrohit/mellow.nvim',
+    priority = 1000,
+    lazy = false,
+    config = function()
+      vim.cmd.colorscheme 'mellow'
+    end,
+  },
 }
