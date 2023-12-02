67e7cc943566c5b0881323fe593f859b80573a6d
Sam Walker <sam.walker@aviatnet.com>
2023-12-02 17:40:10 +1300

    Add zen and search nvim plugins

--- nvim/lua/swalker/plugins/misc.lua
+++ nvim/lua/swalker/plugins/misc.lua
@@ -1,5 +1,5 @@
 return {
-  -- Detect tabstop and shiftwidth automatically
+  -- Detect tatically
   'tpope/vim-sleuth',
 
   -- Additional lua configuration, makes nvim stuff amazing!
@@ -13,5 +13,30 @@ return {
   {
     'christoomey/vim-tmux-navigator',
     lazy = false,
+  },
+
+  {
+    'folke/todo-comments.nvim',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    },
+    {
+      -- Better search jumplist
+      'folke/flash.nvim',
+      event = 'VeryLazy',
+      ---@type Flash.Config
+      opts = {},
+      -- stylua: ignore
+      keys = {
+        { "s",     mode = { "n", "x", "o" }, function() require("flash").jump() end,              desc = "Flash" },
+        { "S",     mode = { "n", "x", "o" }, function() require("flash").treesitter() end,        desc = "Flash Treesitter" },
+        { "r",     mode = "o",               function() require("flash").remote() end,            desc = "Remote Flash" },
+        { "R",     mode = { "o", "x" },      function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+        { "<c-s>", mode = { "c" },           function() require("flash").toggle() end,            desc = "Toggle Flash Search" },
+      },
+    },
   },
 }
