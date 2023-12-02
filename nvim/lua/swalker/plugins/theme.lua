d3c48e58b20fa9427d3d78b12bca3ed2d23b77e6
Sam Walker <sam.walker@aviatnet.com>
2023-12-02 15:49:18 +1300

    Tweak color scheme

--- nvim/lua/swalker/plugins/theme.lua
+++ nvim/lua/swalker/plugins/theme.lua
@@ -5,10 +5,14 @@ return {
     priority = 1000,
     config = function()
       require('catppuccin').setup {
-        -- transparent_background = true,
+        transparent_background = true,
         compile_path = vim.fn.stdpath 'cache' .. '/catppuccin',
       }
+
       vim.cmd 'colorscheme catppuccin-macchiato'
+
+      -- vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
+      -- vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
     end,
   },
 }
